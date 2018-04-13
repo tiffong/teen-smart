3116717c792173e90ad64e1d40b368ec46b508f7
Sajana Hemandra Weerawardhena <sajana@stanford.edu>
2018-04-13 15:19:19 -0700

    simple node server

---
+++ backend-teensmart/webServer.js
@@ -0,0 +1,66 @@
+"use strict";
+
+/* jshint node: true */
+
+/*
+ * This exports the current directory via webserver listing on a hard code (3000) port. It also
+ * establishes a connection to the MongoDB named 'CS50Project'.
+ *
+ * To start the webserver run the command:
+ *    node webServer.js
+ *
+ * If using MongoDB, make sure to run "mongod" before running "node webServer.js"
+ *
+ * Note that anyone able to connect to localhost:portNo will be able to fetch any file accessible
+ * to the current user in the current directory or any of its children.
+ *
+ * This webServer exports the following URLs:
+ * /              -  Returns a text status message.  Good for testing web server running.
+ * /test          - (Same as /test/info)
+ * /test/info     -  Returns the SchemaInfo object from the database (JSON format).  Good
+ *                   for testing database connectivity.
+ * /test/counts   -  Returns the population counts of the CS50 collections in the database.
+ *                   Format is a JSON object with properties being the collection name and
+ *                   the values being the counts.
+ *
+ */
+
+/********************************************************************************************/
+/***************************************    SET UP     **************************************/
+/********************************************************************************************/
+
+/*** INFO: "require" statements are Node.js's version of import statements ***/
+var mongoose = require('mongoose');
+var async = require('async');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+
+
+// Load Express
+var express = require('express');
+var app = express();
+
+
+// We have the express static module (http://expressjs.com/en/starter/static-files.html) do all
+// the work for us. This EXPORTS your current working directory that webServer.js is in. (__dirname)
+app.use(express.static(__dirname));
+
+
+
+/************************************************************************************************/
+/*******************************     REST API Back End CALLS     ********************************/
+/************************************************************************************************/
+/*
+ * Example #1: Simple GET Request
+ * When making an AJAX GET request to the path '/', the web server responds with a simple message.
+ */
+app.get('/', function (request, response) {
+    response.send('Simple web server of files from ' + __dirname);
+});
+
+
+// DO NOT DELETE: Opens port for loading your webserver locally
+var server = app.listen(3000, function () {
+    var port = server.address().port;
+    console.log('Listening at http://localhost:' + port + ' exporting the directory ' + __dirname);
+});
