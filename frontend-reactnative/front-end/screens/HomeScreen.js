fd74baab9af44e288977ecc5f5f942d7e05f8710
Paola martinez <paomart@stanford.edu>
2018-04-30 13:14:01 -0700

    Home Page
    I got the layout of the homepage setup. The next step is to connect the buttons to the surveys.

--- frontend-reactnative/front-end/screens/HomeScreen.js
+++ frontend-reactnative/front-end/screens/HomeScreen.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import { Button } from 'react-native';
+
 import {
   Image,
+  AppRegistry,
   Platform,
   ScrollView,
   StyleSheet,
   Text,
   TouchableOpacity,
+  TouchableHighlight,
   View,
 } from 'react-native';
 import { WebBrowser } from 'expo';
 
 import { MonoText } from '../components/StyledText';
 
+import { StackNavigator } from 'react-navigation';
+
 export default class HomeScreen extends React.Component {
+  constructor(props) {
+  super(props)
+  this.state = { count: 0 }
+}
+
   static navigationOptions = {
     header: null,
   };
@@ -20,98 +31,89 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
           <View style={styles.welcomeContainer}>
+            <Text style={styles.developmentModeText}> Bienvenido </Text>
+
             <Image
               source={
                 __DEV__
-                  ? require('../assets/images/robot-dev.png')
+                  ? require('../assets/images/complete_logo.png')
                   : require('../assets/images/robot-prod.png')
               }
               style={styles.welcomeImage}
             />
-          </View>
 
+          </View>
           <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
 
-            <Text style={styles.getStartedText}>Get started by opening</Text>
+            <Text style={styles.getStartedText}>VIVE TU JUVENTUD. CUIDA TU SALUD. </Text>
 
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
+            <Text style={styles.description}>Obten una mejor experiencia al llenar las siguientes encuestas:. </Text>
           </View>
 
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
+          <TouchableOpacity
+            style={styles.firstButton}
+            onPress={() =>  this.props.navigation.navigate('Links')}
+            underlayColor="gray"
+          >
+            <Text> Primera Encuesta </Text>
+          </TouchableOpacity>
 
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
+          <TouchableOpacity
+            style={styles.firstButton}
+            onPress={() =>  this.props.navigation.navigate('Links')}
+          >
+            <Text> Segunda Encuesta </Text>
+          </TouchableOpacity>
 
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
+          <TouchableOpacity
+            style={styles.firstButton}
+            onPress={() =>  this.props.navigation.navigate('Links')}
+          >
+            <Text> Tercera Encuesta </Text>
+          </TouchableOpacity>
       </View>
     );
   }
 
   _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
+    return (
+      <Text style={styles.developmentModeText}>
+        Bienvenido
+      </Text>
     );
-  };
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    paddingTop: 30
+  },
+  firstButton: {
+    alignItems: 'center',
+    marginBottom: 40,
+    padding: 20,
+    width: 250,
+    backgroundColor: '#ee8424',
+    borderRadius: 5
   },
   developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
+    marginTop: 10,
+    fontSize: 25,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 25,
     textAlign: 'center',
   },
-  contentContainer: {
-    paddingTop: 30,
+  description: {
+    marginTop: 30,
+    marginBottom: 20,
+    color: 'grey',
+    fontSize: 13,
+    lineHeight: 13,
+    textAlign: 'center',
   },
   welcomeContainer: {
     alignItems: 'center',
@@ -119,10 +121,10 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   welcomeImage: {
-    width: 100,
-    height: 80,
+    width: 250,
+    height: 100,
     resizeMode: 'contain',
-    marginTop: 3,
+    marginTop: 1,
     marginLeft: -10,
   },
   getStartedContainer: {
@@ -146,43 +148,8 @@ const styles = StyleSheet.create({
     lineHeight: 24,
     textAlign: 'center',
   },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
   navigationFilename: {
     marginTop: 5,
   },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+
 });
