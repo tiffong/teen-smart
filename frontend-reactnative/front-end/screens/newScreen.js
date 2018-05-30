feec8810c5dc38523d32e93626cb1155919a4bd7
Sajana Hemandra Weerawardhena <sajana@stanford.edu>
2018-05-30 13:07:55 -0700

    survery

--- frontend-reactnative/front-end/screens/newScreen.js
+++ frontend-reactnative/front-end/screens/newScreen.js
@@ -5,78 +5,114 @@ import { Button } from 'react-native';
 
 
 export default class GlobalSurvey extends React.Component {
-  static navigationOptions = {
-    title: 'Survey',
-  };
+    static navigationOptions = {
+        title: 'Survey',
+    };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      questions:  [{question: "Tomaré alcohol el próximo año.",
-                     answer: ["No probable del todo", "Poco probable", "Algo probable", "Muy probable"]},
+    constructor(props) {
+        super(props);
+        this.state = {
+            questions:  [{question: "Tomaré alcohol el próximo año.",
+                answer: ["No probable del todo", "Poco probable", "Algo probable", "Muy probable"]},
 
-                   {question: "Fumaré cigarrillos dentro de un año",
+                {question: "Fumaré cigarrillos dentro de un año",
                     answer : ["No probable del todo", "Poco probable", "Algo probable", "Muy probable"]}
 
+            ],
+            //answer: [{"Tomaré alcohol el próximo año." : "Muy probable" }],
+            answer: {},
+            currentQuestion: 0
+        }
 
 
-                 ],
-      //answer: {},
-      currentQuestion: 0
+    }
+
+    componentDidMount(){
+      //get the survey database
+      //spinner till then
     }
 
 
-  }
 
-  componentWillReceiveProps(){
-    console.log("Location new props")
 
-  }
-  nextQuestion() {
-    this.setState(previousState => {
-      previousState.currentQuestion += 1;
-      return previousState;
-      }
-    );
-  }
+    nextQuestion() {
+        let nextQuestionIndex = this.state.currentQuestion;
+        if (this.state.currentQuestion < (this.state.questions.length-1)){
+            nextQuestionIndex += 1;
+        }
+        this.setState(previousState => {
+                previousState.currentQuestion = nextQuestionIndex;
+                return previousState;
+            }
+        );
+    }
 
-  prevQuestion() {
+    prevQuestion() {
+        let lastQuestionIndex = this.state.currentQuestion;
+        if (this.state.currentQuestion > 0){
+            lastQuestionIndex -= 1;
+        }
+        this.setState(previousState => {
+                previousState.currentQuestion = lastQuestionIndex;
+                return previousState;
+            }
+        );
+    }
 
-  }
+    updateAnswer(question, answer) {
+        this.setState(previousState => {
+                previousState.answer[question] = answer[0];
+                return previousState;
+            }
+        );
+    }
 
-  updateAnswer(question, answer) {
-    this.setState(previousState => {
-      previousState.answer[question] = answer;
-      return previousState;
-      }
-    );
-  }
+    submit() {
 
-  submit() {
+    }
+    render() {
+        let lastquestion = false;
+        let firstquestion = false;
+        if (this.state.currentQuestion === this.state.questions.length-1) {
+            lastquestion = true;
+        }
 
-  }
-  render() {
-    console.log("Hi")
-    console.log(this.state.questions[this.state.currentQuestion])
-    return (
-      <View>
-      <SurveyPage nextQuestion={this.nextQuestion.bind(this)}
-      prevQuestion={this.prevQuestion.bind(this)}
-      updateAnswer={this.updateAnswer}
-      submit={this.submit}
-      question={this.state.questions[this.state.currentQuestion]}
-      style={styles.container}/>
+        if (this.state.currentQuestion === 0) {
+            firstquestion = true;
+        }
+        let question = this.state.questions[this.state.currentQuestion].question;
+        console.log('Dividerr');
+        console.log(question);
+        let selectedAnswer = ""
+        if(question in this.state.answer){
+            console.log('Question in answer');
+            selectedAnswer =  this.state.answer[question];
+        }
+        console.log('location');
+        console.log(this.state.answer)
+        console.log(selectedAnswer)
+        return (
+            <View>
+              <SurveyPage nextQuestion={this.nextQuestion.bind(this)}
+                          prevQuestion={this.prevQuestion.bind(this)}
+                          updateAnswer={this.updateAnswer.bind(this)}
+                          submit={this.submit}
+                          question={this.state.questions[this.state.currentQuestion]}
+                          style={styles.container}
+                          lastquestion={lastquestion}
+                          firstquestion={firstquestion}
+                          selectedAnswer={selectedAnswer}
+              />
+            </View>
 
-    </View>
-
-    );
-  }
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
+    container: {
+        flex: 1,
+        paddingTop: 15,
+        backgroundColor: '#fff',
+    },
 });
--- frontend-reactnative/front-end/screens/surveyPage.js
+++ frontend-reactnative/front-end/screens/surveyPage.js
@@ -1,67 +1,96 @@
 import React from 'react';
 import {
-   TouchableOpacity,
-   Button,
-   Text,
-   View,
-   ScrollView,
-   StyleSheet
+    TouchableOpacity,
+    Button,
+    Text,
+    View,
+    ScrollView,
+    StyleSheet
 
- } from 'react-native';
+} from 'react-native';
 
 import CustomMultiPicker from "react-native-multiple-select-list";
 
 export default class SurveyPage extends React.Component {
-  constructor(props){
-    console.log("Hi")
-    super(props);
-    this.state = {
-      question: this.props.question["question"],
-      answer: this.props.question["answer"]
+    constructor(props){
+        super(props);
+        this.state = {
+            question: this.props.question["question"],
+            answer: this.props.question["answer"],
+            updateAnswer: this.props.updateAnswer
+        }
     }
-  }
+
+    componentWillReceiveProps(props){
+        this.setState(previousState => {
+                previousState.question = props.question["question"];
+                previousState.answer = props.question["answer"];
+                return previousState;
+            }
+        );
+    }
+    renderNextQuestion(){
+        if (this.props.lastquestion) {
+            return (<TouchableOpacity
+                        onPress={this.props.nextQuestion}
+                        underlayColor="gray">
+                        <Text> Submit </Text>
+                    </TouchableOpacity>);
+        }
+        return(
+            <TouchableOpacity
+                onPress={this.props.nextQuestion}
+                underlayColor="gray">
+                <Text> Next Question </Text>
+            </TouchableOpacity>
+        );
+    }
+
+    renderLastQuestion(){
+        if (this.props.firstquestion) return "";
+        return(
+            <TouchableOpacity
+                onPress={this.props.prevQuestion}
+                underlayColor="gray">
+                <Text> Last Question </Text>
+            </TouchableOpacity>
+        );
+    }
 
 
+    render() {
+        console.log('Holocene');
+        console.log(this.props.selectedAnswer);
+        console.log('Part from me');
+        return (
+
+            <View>
+                <Text> {this.state.question} </Text>
+                <CustomMultiPicker
+                    options={this.state.answer}
+                    search={false} // should show search bar?
+                    multiple={false} //
+                    placeholder={"Search"}
+                    placeholderTextColor={'#757575'}
+                    returnValue={"label"} // label or value
+                    callback={(res)=> {this.state.updateAnswer(this.state.question,res)}} // callback, array of selected items
+                    rowBackgroundColor={"#eee"}
+                    rowHeight={40}
+                    rowRadius={5}
+                    iconColor={"#00a2dd"}
+                    iconSize={30}
+                    selectedIconName={"ios-checkmark-circle-outline"}
+                    unselectedIconName={"ios-radio-button-off-outline"}
+                    scrollViewHeight={240}
+                    selected={this.props.selectedAnswer} // list of options which are selected by default
+                />
+                <View>
+                    {this.renderLastQuestion()}
+                    {this.renderNextQuestion()}
+                </View>
 
 
-  render() {
-    return (
-
-      <View>
-        //You have to connect this to the newscreen (so the answer is updated as need.)
-        //Also set up navigation buttons to go back and forward
-        //Question 1 Text:
-        <Text> {this.state.question} </Text>
-        //Question 1
-        <CustomMultiPicker
-        options={this.state.answer}
-        search={false} // should show search bar?
-        multiple={false} //
-        placeholder={"Search"}
-        placeholderTextColor={'#757575'}
-        returnValue={"label"} // label or value
-        callback={(res)=>{ console.log(res) }} // callback, array of selected items
-        rowBackgroundColor={"#eee"}
-        rowHeight={40}
-        rowRadius={5}
-        iconColor={"#00a2dd"}
-        iconSize={30}
-        selectedIconName={"ios-checkmark-circle-outline"}
-        unselectedIconName={"ios-radio-button-off-outline"}
-        scrollViewHeight={240}
-        selected={[1,2]} // list of options which are selected by default
-      />
-      <View>
-      <TouchableOpacity
-              onPress={this.props.nextQuestion}
-              underlayColor="gray"
-            >
-              <Text> Next Q </Text>
-      </TouchableOpacity>
-      </View>
-
-
-    </View>
-  );
-  }
+            </View>
+        );
+    }
 }
